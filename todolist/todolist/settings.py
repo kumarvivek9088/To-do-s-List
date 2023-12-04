e5b513a7566ce96bd47b91c6741f29ef695d82ca
Vivek Kumar <94466635+kumarvivek9088@users.noreply.github.com>
2023-12-04 11:17:49 +0530

    Update settings.py

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -74,10 +74,19 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'web',
+        'PASSWORD' : "Vivek@203001",
+        'USER': 'root',
+        'HOST' : 'localhost',
     }
 }
 
